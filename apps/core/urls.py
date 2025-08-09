212e62a10d3b636b1c915c7d700160ef4bd7fad8
7nimor <nima7playstation4@gmail.com>
2025-08-09 12:45:06 +0330

    Add field image to poultryinspection

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,12 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
-from apps.views import jsdhf
-
 router = DefaultRouter()
 app_name = "core"
 
 urlpatterns = [
-    path('core/', include(router.urls)),
-    path('jsdhf/', jsdhf),
+    path('core/', include(router.urls))
 ]
