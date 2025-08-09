480a0d7dc34240a235d4bb2f5f51c88f296187ed
7nimor <nima7playstation4@gmail.com>
2025-08-09 12:03:24 +0330

    Add field image to poultryinspection

--- apps/poultry_inspection/models.py
+++ apps/poultry_inspection/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 from apps.authentication import models as auth_models
+from apps.authentication.models import Province, City
 from apps.core.models import BaseModel
-from apps.authentication.models import Province,City
 
 
 class Poultry(BaseModel):
@@ -50,20 +50,19 @@ class PoultryInspection(BaseModel):
         related_name='user_inspection',
         null=True
     )
-    business_license = models.CharField(max_length=100, null=True)
-    business_license_issuer = models.CharField(max_length=100, null=True)
-    licence_number = models.IntegerField(default=0)
-    registration_number = models.IntegerField(default=0)
-    economic_code = models.IntegerField(default=0)
-    type_of_ownership = models.CharField(max_length=250,null=True)
-    unit_type = models.CharField(max_length=250,null=True)
-    #todo بازرس همراه چیه
     poultry = models.ForeignKey(
         Poultry,
         on_delete=models.CASCADE,
         related_name='poultry_inspection',
         null=True
     )
+    business_license = models.CharField(max_length=100, null=True)
+    business_license_issuer = models.CharField(max_length=100, null=True)
+    licence_number = models.IntegerField(default=0)
+    registration_number = models.IntegerField(default=0)
+    economic_code = models.IntegerField(default=0)
+    type_of_ownership = models.CharField(max_length=250, null=True)
+    unit_type = models.CharField(max_length=250, null=True)
+    # todo بازرس همراه چیه
     date = models.DateTimeField(null=True)
-
-
+    image = models.TextField(null=True)
